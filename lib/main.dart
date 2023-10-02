831d61987bc1cd2e5efb20cd4d29e18d187c4d96
tran-haison <haison250199@gmail.com>
2023-10-02 10:15:24 +0700

    feat: Build web

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  usePathUrlStrategy();
   runApp(const App());
 }
